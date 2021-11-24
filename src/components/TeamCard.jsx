c42367f9fd3403477790a8be76d84cccaaf666f1
Noah Johnson <jnoahjohnson@gmail.com>
2021-11-24 13:01:50 -0700

    Add popup for team

---
+++ src/components/TeamCard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { XIcon } from "@heroicons/react/outline";
+
+export default function TeamCard({ name, headshot, bio }) {
+  let [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <div className="relative w-full h-96 flex items-end p-6 transition-all duration-300 group">
+        <img
+          src={headshot}
+          className="object-cover w-full h-full absolute inset-0"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-800 to-gray-400 mix-blend-multiply hover:bg-sushi transition-all duration-300 opacity-0 group-hover:opacity-50"></div>
+        <div className="relative opacity-0 group-hover:opacity-100 transition duration-150">
+          <h1 className="text-3xl font-bold mb-1">{name}</h1>
+          <p className="text-gray-300 mb-2">{bio}</p>
+          <button onClick={() => setIsOpen(true)} className="text-white">
+            See More
+          </button>
+        </div>
+      </div>
+      <div
+        className={`${isOpen ? "fixed" : "hidden"} inset-0 w-full h-full z-30`}
+      >
+        <div
+          className="absolute w-full h-full bg-gray-500 opacity-20"
+          onClick={() => setIsOpen(false)}
+        ></div>
+        <div className="absolute inset-0 md:w-2/3 md:h-2/3 bg-darkbg m-auto p-4 shadow-lg">
+          <XIcon
+            onClick={() => setIsOpen(false)}
+            className="text-white absolute top-4 right-4 w-8 h-8 hover:text-gray-300 cursor-pointer"
+          >
+            X
+          </XIcon>
+          <div className="flex flex-col md:flex-row w-full h-full pt-12 md:p-0">
+            <div className="relative w-full h-96 md:h-full md:w-96 mr-4 mb-2 md:mb-0">
+              <img
+                src={headshot}
+                className="w-full h-full object-cover object-center absolute inset-0"
+              />
+            </div>
+            <aside className="flex flex-col items-start justify-start">
+              <h1 className="text-5xl font-bold mb-1">{name}</h1>
+              <p className="mb-2 text-2xl">Social Links!</p>
+              <p className="text-2xl">You can put the bio here!</p>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
