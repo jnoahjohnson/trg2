917e4e2f945b291a255bef20c18ec0dc9bc295d9
Samantha Berg <sb11158@byu.edu>
2021-12-08 20:49:54 -0700

    fixed team page and added changes to news page

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -7,18 +7,7 @@ export default function BlogSection({ title, href, category, description, date, 
         <div className="h-1/3 sm:h-2/3" />
       </div>
       <div className="relative max-w-7xl mx-auto">
-        <div className="text-center">
-          <h2 className="text-3xl tracking-tight font-extrabold text-white sm:text-4xl">
-            News
-          </h2>
-          <br></br>
-          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
-            libero labore natus atque, ducimus sed.
-          </p>
-        </div>
         <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-          {/* {posts.map((post) => ( */}
             <div
               key={title}
               className="flex flex-col rounded-lg shadow-lg overflow-hidden"
