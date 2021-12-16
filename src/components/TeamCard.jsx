8179502f9849f15e9d715041386c584e1e30e011
Samantha Berg <sb11158@byu.edu>
2021-12-15 20:11:20 -0700

    added linkedin to team/still need to line up

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,7 +3,6 @@ import { XIcon } from "@heroicons/react/outline";
 
 export default function TeamCard({ name, headshot, title, bio }) {
   let [isOpen, setIsOpen] = useState(false);
-  console.log({title})
   return (
     <div>
       <div className="relative w-full h-96 flex items-end p-6 transition-all duration-300 group">
@@ -42,9 +41,29 @@ export default function TeamCard({ name, headshot, title, bio }) {
               />
             </div>
             <aside className="flex flex-col items-start justify-start">
-              <h1 className="text-5xl font-bold mb-1">{name}</h1>
+              <div className="grid grid-col-2 gap-4">
+                  <div>
+                    <h1 className="text-5xl font-bold mb-1">{name}</h1>
+                  </div>
+                  <div>
+                    <a href="#" class="text-gray-300 hover:text-gray-500">
+                    <span class="sr-only">LinkedIn</span>
+                    <svg
+                      class="h-10 w-10"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    </a>
+                  </div>
+              </div>
               <br />
-              <p className="mb-2 text-2xl">Social Links!</p>
               <p className="mb-2 text-2xl">{title}</p>
               <p className="text-2xl">{bio}</p>
             </aside>
