9575cb799dffc612ad59862dc5d98bffcfeeec2d
Samantha Berg <sb11158@byu.edu>
2022-01-06 21:17:27 -0700

    project updates

--- src/utils/getIcon.jsx
+++ src/utils/getIcon.jsx
@@ -9,20 +9,29 @@ import {
   OfficeBuildingIcon,
   CurrencyDollarIcon, 
   ViewBoardsIcon,
+  ClipboardListIcon,
 } from "@heroicons/react/outline";
 
 export default function getIcon(type) {
   switch (type) {
     case "units":
       return <HashtagIcon />;
+    case "keys":
+      return <HashtagIcon />;
     case "startYear":
+      return <CalendarIcon />;
+    case "completedYear":
       return <CalendarIcon />;
     case "status":
       return <OfficeBuildingIcon />;
     case "developmentalBudget":
       return <CurrencyDollarIcon />;
+    case "location":
+      return <LocationMarkerIcon />;
     case "squareFeet":
       return <ViewBoardsIcon />;
+    case "type":
+      return <ClipboardListIcon />;
     default:
       return <CogIcon />;
   }
